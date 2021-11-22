fd2ca98408b8cf3566ba26a97c8de59ce0745fad
Mahalinoro Razafimanjato <m.razafiman@alustudent.com>
2021-11-22 20:18:25 +0300

    added basic components + updated metadate

---
+++ client/src/components/authentication.js
@@ -0,0 +1,143 @@
+import React from "react";
+ 
+// We import bootstrap to make our application look better.
+// import "bootstrap/dist/css/bootstrap.css";
+import '../assets/css/auth.css';
+
+// We import material icons for the page
+// import { ShoppingCartOutlinedIcon } from '@mui/icons-material/ShoppingCartOutlined';
+// import { FavoriteBorderOutlinedIcon } from '@mui/icons-material/FavoriteBorderOutlined';
+// import { SearchOutlinedIcon } from '@mui/icons-material/SearchOutlined';
+
+// We import NavLink to utilize the react router.
+import { NavLink } from "react-router-dom";
+ 
+class Authentication extends React.Component {
+  constructor(props) {
+      super(props);
+      this.state = {isLogin: false};
+  }
+
+  handleClickLogin() {
+      this.setState({isLogin: false});
+  }
+
+  handleClickSignUp() {
+      this.setState({isLogin: true});
+  }
+
+  render() {
+    let modal;
+    if(this.state.isLogin){
+        modal = (
+        <div className='modal'>
+        <div className="login-container">
+            <p className="text-medium-36 text-color">Welcome Back!</p>
+            <form>
+              <div className="email-box">
+                  <input type="email" placeholder="Email" className="email-input text-regular-16"></input>
+                  <i aria-hidden="true">
+                    <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000">
+                      <path d="M0 0h24v24H0V0z" fill="none"/>
+                      <path d="M22 6c0-1.1-.9-2-2-2H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6zm-2 0l-8 5-8-5h16zm0 12H4V8l8 5 8-5v10z"/>
+                    </svg>                      
+                  </i>
+              </div>  
+
+              <div style={{margin: "16px"}}></div>
+
+              <div className="email-box">
+                  <input type="password" placeholder="Password" className="email-input text-regular-16"></input>
+                  <i aria-hidden="true">
+                      <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><g fill="none">
+                        <path d="M0 0h24v24H0V0z"/><path d="M0 0h24v24H0V0z" opacity=".87"/>
+                        </g><path d="M18 8h-1V6c0-2.76-2.24-5-5-5S7 3.24 7 6v2H6c-1.1 0-2 .9-2 2v10c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V10c0-1.1-.9-2-2-2zM9 6c0-1.66 1.34-3 3-3s3 1.34 3 3v2H9V6zm9 14H6V10h12v10zm-6-3c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2z"/>
+                      </svg>
+                  </i>
+              </div>     
+
+              <div style={{margin: "16px"}}></div>
+
+              <input type="submit" className="login-btn text-medium-16" value="Login"></input>
+            </form>
+            <div style={{margin: "16px"}}></div>
+            <NavLink to="/" className="text-regular-16 text-color forgot">forgot your password?</NavLink>
+            <p className="text-medium-16 text-color">OR</p>
+        
+            {/* <button>Continue with Google</button>
+            <button>Continue with Facebook</button> */}
+            <div className="divider"></div>
+            <p className="text-regular-16 text-color">Don’t have an account? <button className="signup-btn text-medium-16" onClick={() => {this.handleClickLogin()}}>Sign Up</button></p>
+            
+        </div>
+    </div>
+
+      );
+
+      }else{       
+        modal = (
+          <div className='modal'>
+        <div className="login-container">
+            <p className="text-medium-36 text-color">Create An Account</p>
+            <form>
+               <div className="email-box">
+                  <input type="text" placeholder="Name" className="email-input text-regular-16"></input>
+                  <i aria-hidden="true">
+                    <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000">
+                      <path d="M0 0h24v24H0V0z" fill="none"/>
+                      <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zM7.07 18.28c.43-.9 3.05-1.78 4.93-1.78s4.51.88 4.93 1.78C15.57 19.36 13.86 20 12 20s-3.57-.64-4.93-1.72zm11.29-1.45c-1.43-1.74-4.9-2.33-6.36-2.33s-4.93.59-6.36 2.33C4.62 15.49 4 13.82 4 12c0-4.41 3.59-8 8-8s8 3.59 8 8c0 1.82-.62 3.49-1.64 4.83zM12 6c-1.94 0-3.5 1.56-3.5 3.5S10.06 13 12 13s3.5-1.56 3.5-3.5S13.94 6 12 6zm0 5c-.83 0-1.5-.67-1.5-1.5S11.17 8 12 8s1.5.67 1.5 1.5S12.83 11 12 11z"/>
+                    </svg>                   
+                  </i>
+              </div>  
+
+              <div style={{margin: "16px"}}></div>
+
+              <div className="email-box">
+                  <input type="email" placeholder="Email" className="email-input text-regular-16"></input>
+                  <i aria-hidden="true">
+                    <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000">
+                      <path d="M0 0h24v24H0V0z" fill="none"/>
+                      <path d="M22 6c0-1.1-.9-2-2-2H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6zm-2 0l-8 5-8-5h16zm0 12H4V8l8 5 8-5v10z"/>
+                    </svg>                      
+                  </i>
+              </div>  
+
+              <div style={{margin: "16px"}}></div>
+
+              <div className="email-box">
+                  <input type="password" placeholder="Password" className="email-input text-regular-16"></input>
+                  <i aria-hidden="true">
+                      <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><g fill="none">
+                        <path d="M0 0h24v24H0V0z"/><path d="M0 0h24v24H0V0z" opacity=".87"/>
+                        </g><path d="M18 8h-1V6c0-2.76-2.24-5-5-5S7 3.24 7 6v2H6c-1.1 0-2 .9-2 2v10c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V10c0-1.1-.9-2-2-2zM9 6c0-1.66 1.34-3 3-3s3 1.34 3 3v2H9V6zm9 14H6V10h12v10zm-6-3c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2z"/>
+                      </svg>
+                  </i>
+              </div>     
+
+              <div style={{margin: "16px"}}></div>
+
+              <input type="submit" className="login-btn text-medium-16" value="Sign Up"></input>
+            </form>
+            <div style={{margin: "16px"}}></div>
+            <NavLink to="/" className="text-regular-16 text-color forgot">forgot your password?</NavLink>
+            <p className="text-medium-16 text-color">OR</p>
+        
+            {/* <button>Continue with Google</button>
+            <button>Continue with Facebook</button> */}
+            <div className="divider"></div>
+            <p className="text-regular-16 text-color">Already have an account? <button className="signup-btn text-medium-16" onClick={() => {this.handleClickSignUp()}}>Login</button></p>
+            
+        </div>
+    </div>
+
+        );
+      
+      
+    }
+    return (
+      modal
+    );
+  }
+}
+ 
+export default Authentication;
---
+++ client/src/components/navbar.js
@@ -0,0 +1,73 @@
+import React from "react";
+ 
+// We import bootstrap to make our application look better.
+// import "bootstrap/dist/css/bootstrap.css";
+import '../assets/css/nav.css';
+
+// We import material icons for the page
+// import { ShoppingCartOutlinedIcon } from '@mui/icons-material/ShoppingCartOutlined';
+// import { FavoriteBorderOutlinedIcon } from '@mui/icons-material/FavoriteBorderOutlined';
+// import { SearchOutlinedIcon } from '@mui/icons-material/SearchOutlined';
+
+// We import NavLink to utilize the react router.
+import { NavLink } from "react-router-dom";
+ 
+// Here, we display our Navbar
+const Navbar = () => {
+  return (
+    <nav>
+        <div className="nav-container">
+            <div className="nav-link-items">
+                <div className="logo-item">
+                    <NavLink to="/" className="logo">Kugu</NavLink>
+                </div>
+                <div className="nav-item text-regular-20">
+                    <NavLink to="/" className="text-color">SELL</NavLink>
+                </div>                    
+                <div className="nav-item text-regular-20">
+                    <NavLink to="/" className="text-color">ABOUT</NavLink>
+                </div>
+            </div>
+
+            <div className="nav-search">
+                <form>
+                    <div className="search-box">
+                        <input type="search" placeholder="Search for Item" className="search-input text-regular-16"></input>
+                        <i aria-hidden="true">
+                            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000">
+                                <path d="M0 0h24v24H0V0z" fill="none"/>
+                                <path d="M15.5 14h-.79l-.28-.27c1.2-1.4 1.82-3.31 1.48-5.34-.47-2.78-2.79-5-5.59-5.34-4.23-.52-7.79 3.04-7.27 7.27.34 2.8 2.56 5.12 5.34 5.59 2.03.34 3.94-.28 5.34-1.48l.27.28v.79l4.25 4.25c.41.41 1.08.41 1.49 0 .41-.41.41-1.08 0-1.49L15.5 14zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"/>
+                            </svg>
+                        </i>
+                    </div>                       
+                </form>
+            </div>
+
+            <div className="nav-icons-buttons">                    
+                <div className="nav-icon">
+                    <NavLink to="/">
+                        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000">
+                            <path d="M0 0h24v24H0V0z" fill="none"/>
+                            <path d="M16.5 3c-1.74 0-3.41.81-4.5 2.09C10.91 3.81 9.24 3 7.5 3 4.42 3 2 5.42 2 8.5c0 3.78 3.4 6.86 8.55 11.54L12 21.35l1.45-1.32C18.6 15.36 22 12.28 22 8.5 22 5.42 19.58 3 16.5 3zm-4.4 15.55l-.1.1-.1-.1C7.14 14.24 4 11.39 4 8.5 4 6.5 5.5 5 7.5 5c1.54 0 3.04.99 3.57 2.36h1.87C13.46 5.99 14.96 5 16.5 5c2 0 3.5 1.5 3.5 3.5 0 2.89-3.14 5.74-7.9 10.05z"/>
+                        </svg>
+                    </NavLink>
+                </div>
+                    <div className="nav-icon">
+                    <NavLink to="/">
+                        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000">
+                            <path d="M0 0h24v24H0V0z" fill="none"/>
+                            <path d="M15.55 13c.75 0 1.41-.41 1.75-1.03l3.58-6.49c.37-.66-.11-1.48-.87-1.48H5.21l-.94-2H1v2h2l3.6 7.59-1.35 2.44C4.52 15.37 5.48 17 7 17h12v-2H7l1.1-2h7.45zM6.16 6h12.15l-2.76 5H8.53L6.16 6zM7 18c-1.1 0-1.99.9-1.99 2S5.9 22 7 22s2-.9 2-2-.9-2-2-2zm10 0c-1.1 0-1.99.9-1.99 2s.89 2 1.99 2 2-.9 2-2-.9-2-2-2z"/>
+                        </svg>
+                    </NavLink>
+                </div>
+                    <div className="nav-icon text-regular-16">
+                    <NavLink to="/" className="nav-button primary-color">Sign Up | Login</NavLink>
+                </div>                
+            </div>
+        </div>
+    </nav>
+  
+  );
+};
+ 
+export default Navbar;
