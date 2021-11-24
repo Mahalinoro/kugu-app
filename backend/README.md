9f9484e6b4361da9ff597699668a5ef2ad85bbfc
Audrey-Njenga <a.njenga@alustudent.com>
2021-11-24 16:50:10 +0300

    Backend boilerplate and auth

---
+++ backend/README.md
@@ -0,0 +1,3 @@
+#### To start on /backend  
+#### npm start
+#### go to browser on localhost://3000/login
---
+++ backend/controllers/mid/auth.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+const { auth } = require('express-openid-connect');
+
+app.use(
+    auth({
+        issuerBaseURL: process.env.ISSUER_BASE_URL,
+        baseURL: process.env.BASE_URL,
+        clientID: process.env.CLIENT_ID,
+        clientSecret: process.env.SECRET
+    })
+);
+
+const port = process.env.PORT;
+app.listen(port, () => {
+    console.log('App running');
+})
